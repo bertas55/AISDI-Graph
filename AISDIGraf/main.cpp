3f6ec2f7c76743a4c3a9d3f2e0ed1f16e40eb67f
bertas55 <bertas55@gmail.com>
2015-05-27 23:39:08 +0200

    Initial Commit

---
+++ AISDIGraf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AISDIGraf
+//
+//  Created by Hubert on 27.05.2015.
+//  Copyright (c) 2015 Hubert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
